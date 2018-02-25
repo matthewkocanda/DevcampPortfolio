2daf8ef80d3300f2c4c4c75db2270b37c9fb73a5
matthewkocanda <matthewkocanda@gmail.com>
2018-02-25 18:58:47 -0500

    Implemented technologies for the Portfolio model

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,12 @@ puts "5 skills created"
 end
 
 puts "9 portfolio items created"
+
+3.times do |technology|
+  Technology.create!(
+    name: "Technology #{technology}",
+    portfolio_id: Portfolio.last.id
+  )
+end
+
+puts "3 technologies created"
