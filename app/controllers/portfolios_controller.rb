d595ff54a92498a4171739b1ae28325cab9c25ef
matthewkocanda <matthewkocanda@gmail.com>
2018-03-08 14:14:21 -0500

    data added to partials for custom link behaviors

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,6 @@
 class PortfoliosController < ApplicationController
   layout "portfolio"
-  
+
   def index
     @portfolio_items = Portfolio.all
   end
@@ -24,6 +24,7 @@ class PortfoliosController < ApplicationController
 
   def edit
     @portfolio_item = Portfolio.find(params[:id])
+    3.times { @portfolio_item.technologies.build }
   end
 
   def update
