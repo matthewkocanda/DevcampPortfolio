ba2580a72a140464b82a8931e07510bd42fc99b7
matthewkocanda <matthewkocanda@gmail.com>
2018-05-08 19:31:00 -0400

    new push

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,6 +4,10 @@ class Portfolio < ApplicationRecord
   include Placeholder
   validates_presence_of :title, :body, :main_image, :thumb_image
 
+    def self.by_position
+      order("position ASC")
+    end
+
   after_initialize :set_defaults
 
   def set_defaults
