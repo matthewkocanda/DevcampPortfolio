0127750249401092510607fc51c9958bf3f62082
matthewkocanda <matthewkocanda@gmail.com>
2018-06-11 17:23:31 -0400

    fixed stuff

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,6 +1,11 @@
 module PortfoliosHelper
+
+  def image_generator(height:, width:)
+   "http://placehold.it/#{height}x#{width}"
+  end
+
   def portfolio_img img, type
-    if img.model.main_image? || img.model.thumb?
+    if img.model.main_image? || img.model.thumb_image?
       img
     elsif type == 'thumb'
       image_generator(height: '350', width: '200')
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -9,9 +9,15 @@ class Portfolio < ApplicationRecord
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
 
+  def self.angular
+    where(subtitle: 'Angular')
+  end
+
   def self.by_position
     order("position ASC")
   end
+
+  scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails')}
 
 #  after_initialize :set_defaults
 
